0c64b78bc7aa9f7b860c97657421f58e6b5d6bba
alvaroSt <red.fear@hotmail.com>
2022-04-02 17:58:39 -0500

    completado

---
+++ php/envia_correo.php
@@ -0,0 +1,50 @@
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        #Reemplazar este correo por el correo electrónico del destinatario
+        $mail_to = "red.fear@hotmail.com";
+        
+        # Envío de datos
+        
+        $nombre = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["nombre"])));
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $telefono = filter_var(trim($_POST["telefono"], FILTER_SANITIZE_STRING));
+        $mensaje = filter_var(trim($_POST["mensaje"], FILTER_SANITIZE_STRING));
+        
+        if ( empty($nombre) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($telefono) OR empty($mensaje)) {
+            # Establecer un código de respuesta y salida.
+            http_response_code(400);
+            echo "Por favor completa el formulario y vuelve a intentarlo.";
+            exit;
+        }
+        
+        # Contenido del correo
+        $content ="<strong>" . $nombre . "</strong><br>"
+        . $telefono . "<br><br><br>"
+        . $mensaje;
+
+        # Encabezados de correo electrónico.
+        $headers  = 'MIME-Version: 1.0' . "\r\n"
+        .'Content-type: text/html; charset=utf-8' . "\r\n"
+        .'From:' . $nombre . '<' . $email . '>';
+
+        # Envía el correo.
+        $success = mail($mail_to, $subject, $content, $headers);
+        if ($success) {
+            # Establece un código de respuesta 200 (correcto).
+            http_response_code(300);
+            echo "¡Gracias! Tu mensaje ha sido enviado, te responderemos a la brevedad posible.";
+        } else {
+            # Establezce un código de respuesta 500 (error interno del servidor).
+            http_response_code(500);
+            echo "Oops! Algo salió mal, no pudimos enviar tu mensaje.";
+        }
+
+    } else {
+        # No es una solicitud POST, establezce un código de respuesta 403 (prohibido).
+        http_response_code(403);
+        echo "Hubo un problema con tu envío, intenta de nuevo.";
+    }
+
+?>
